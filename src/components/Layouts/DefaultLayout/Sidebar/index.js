e3f5c222e239f3f9705109146c8f4cf6288aba6a
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2023-01-28 12:36:57 +0700

    ⚡Create basic Layout

--- src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,10 @@
+import styles from './Sidebar.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
+
 function Sidebar() {
-    return <h1>Sidebar</h1>
+    return <aside className={cx('wrapper')}>Sidebar</aside>
 }
 
 export default Sidebar
--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,13 +1,18 @@
+import styles from './DefaultLayout.module.scss'
+import classNames from 'classnames/bind'
+
 import Header from '~/components/Layouts/components/Header'
 import Sidebar from './Sidebar'
 
+const cx = classNames.bind(styles)
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header></Header>
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar></Sidebar>
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     )
