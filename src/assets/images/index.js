29e1ec7735dd4fe27c880373bf2b3f8b64822cc3
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2023-01-29 08:52:54 +0700

    ⚡Create Header Layout

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    makeEffect: require('~/assets/images/makeEffect.svg').default,
+}
+
+export default images
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,53 @@
 import classNames from 'classnames/bind'
 import styles from './Header.module.scss'
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faCircleNotch,
+    faCircleXmark,
+    faEllipsisVertical,
+    faMagnifyingGlass,
+    faPlus,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react'
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} className={cx('logo')} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input
+                        type="text"
+                        className={cx('search-bar')}
+                        placeholder="Search account and video"
+                        spellCheck="false"
+                    ></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}></FontAwesomeIcon>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <button className={cx('upload')}>
+                        <FontAwesomeIcon className={cx('upload-icon')} icon={faPlus}></FontAwesomeIcon>
+                        Upload
+                    </button>
+                    <button className={cx('login')}>Login</button>
+                    <button className={cx('make-effect-btn')}>
+                        <img src={images.makeEffect} />
+                    </button>
+                    <button className={cx('options')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical}></FontAwesomeIcon>
+                    </button>
+                </div>
+            </div>
         </header>
     )
 }
